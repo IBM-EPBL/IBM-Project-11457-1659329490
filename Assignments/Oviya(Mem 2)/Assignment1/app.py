cc6524f3dec17163c92d3cf3c1777cb42575b6b3
OviYa <oviya.d@codingmart.com>
2022-10-15 14:32:17 +0530

    Assignment 2 Signup/Sigin

---
+++ Assignments/Oviya(Mem 2)/Assignment1/app.py
@@ -0,0 +1,21 @@
+from . import db, auth
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = "oviii"
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+
+
+db.init_app(app)
+app.register_blueprint(auth.bp)
