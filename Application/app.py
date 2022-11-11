e2aedbe87a6ed2717bd5a8edae93a80531179484
Raveen A <raveen.a@maersk.com>
2022-11-11 11:12:05 +0530

    Initial project setup

---
+++ Application/app.py
@@ -0,0 +1,43 @@
+from routes import auth, dashboard, expenses, budgets, categories, reports, account
+import db
+from flask import Flask, render_template, redirect
+from helpers import login_required, rupees, percent
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = "raveen's_secret"
+#  os.getenv("DATABASE_URL")
+# CSRFProtect(app)
+
+# Custom filter
+app.jinja_env.filters["rupees"] = rupees
+app.jinja_env.filters["percent"] = percent
+
+
+@app.route("/")
+@login_required
+def index():
+    return redirect("/dashboard")
+
+
+@app.route("/design")
+def design():
+    return render_template("view_payer.html", page="reports")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return redirect("/dashboard")
+
+
+db.init_app(app)
+app.register_blueprint(auth.bp)
+app.register_blueprint(dashboard.bp)
+app.register_blueprint(expenses.bp)
+app.register_blueprint(budgets.bp)
+app.register_blueprint(categories.bp)
+app.register_blueprint(reports.bp)
+app.register_blueprint(account.bp)
