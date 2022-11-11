509e63bb6bfc9a691da8422d049239ac8227702f
Raveen2001 <raveenanbarasan@gmail.com>
2022-11-11 17:03:44 +0530

    add category function added

--- Application/utils/categories_utils.py
+++ Application/utils/categories_utils.py
@@ -20,3 +20,20 @@ def get_user_categories(user_id):
 
     return categories
 
+def add_category(name):
+    db.execute(
+        "INSERT INTO categories (name) VALUES (:name)", {
+            "name": name}
+    )
+    db.commit()
+    category_id = db.execute("SELECT id from categories order by ROWID DESC limit 1").fetchone()[0]
+    return category_id
+
+
+# Adds a category to the users account
+def add_category_to_user(category_id, user_id):
+    db.execute(
+        "INSERT INTO usercategories (user_id, category_id) VALUES (:user_id, :category_id)",
+        {"user_id": user_id, "category_id": category_id},
+    )
+    db.commit()
