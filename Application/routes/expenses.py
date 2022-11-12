49609e707c16bee72015aeb35d8904df6338d36d
OviYaD <oviyad.19cse@kongu.edu>
2022-11-12 22:03:44 +0530

    add expense functionality completed

---
+++ Application/routes/expenses.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    session,
+)
+from helpers import login_required
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from utils import expenses_utils, categories_utils, payer_utils, budget_utils
+
+bp = Blueprint("expenses", __name__, url_prefix="/expenses")
+
+engine = create_engine(
+    os.getenv("DATABASE_URL"), connect_args={"check_same_thread": False}
+)
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@bp.route("/add", methods=["GET", "POST"])
+@login_required
+def add_expense():
+    # User reached route via POST
+    if request.method == "POST":
+        formData = dict(request.form.items())
+        expenses_utils.add_expense(formData, session["user_id"])
+
+    categories = categories_utils.get_user_categories(session["user_id"])
+    payers = payer_utils.get_user_payers(session["user_id"])
+    budgets = budget_utils.get_budgets(session["user_id"])
+
+    date = datetime.today().strftime("%Y-%m-%d")
+
+    return render_template(
+        "add_expenses.html",
+        categories=categories,
+        date=date,
+        payers=payers,
+        budgets=budgets,
+    )
